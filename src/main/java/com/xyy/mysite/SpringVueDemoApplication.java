9c8da154f12687f447a6d1111e31d94299e0a52f
xieyuanyang <525254779@qq.com>
2018-04-26 21:29:59 +0800

    添加基础配置

---
+++ src/main/java/com/xyy/mysite/SpringVueDemoApplication.java
@@ -0,0 +1,24 @@
+package com.xyy.mysite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+
+@SpringBootApplication
+public class SpringVueDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVueDemoApplication.class, args);
+	}
+
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+
+		return (container -> {
+			ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+
+			container.addErrorPages(error404Page);
+		});
+	}
+}
